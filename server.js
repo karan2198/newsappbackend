803a016515ad8a028b5c1a9fdd4d019707a16e1b
Karan Raj <karanrajgodansa2003@gmail.com>
2023-07-21 14:52:28 +0530

    setup again devlopment enviourment to production and adding some cool feature

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const http = require('http'); // Change 'https' to 'http'
-const axios = require('axios');
+import express from 'express';
+//import http from 'http'; // Change 'https' to 'http'
+import axios from 'axios';
+import dotenv from 'dotenv';
 
-require('dotenv').config();
+dotenv.config();
 
-const cors = require('cors');
+import cors from 'cors';
 const app = express();
 const apiKey = process.env.API_KEY;
 const port = process.env.PORT || 5000;
 const frontendURL = 'https://main--newsappkaran.netlify.app'; // Replace this with your deployed frontend URL
 
 app.use(cors({
-  origin: frontendURL, // Allow requests from the deployed frontend URL
+  origin:'http://127.0.0.1:5501', // Allow requests from the deployed frontend URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
+
+
 
 app.get('/', (req, res) => {
   res.redirect('/news');
@@ -23,7 +26,13 @@ app.get('/', (req, res) => {
 app.get('/news', async (req, res) => {
   const page = req.query.page || 1;
   const q = req.query.q || 'sports';
-  const url = `https://newsapi.org/v2/everything?q=${q}&from=2023-07-15&to=2023-07-15&pageSize=20&page=${page}&sortBy=popularity&apiKey=${apiKey}`;
+  //const currentDate = new Date().toISOString().slice(0, 10);
+  const today = new Date();
+  const yesterday = new Date(today);
+  yesterday.setDate(today.getDate() - 1);
+  const currentDate = yesterday.toISOString().slice(0, 10);
+
+  const url = `https://newsapi.org/v2/everything?q=${q}&from=${currentDate}&to=${currentDate}&pageSize=20&page=${page}&sortBy=popularity&apiKey=${apiKey}`;
   try {
     const newsResponse = await axios.get(url);
     res.json(newsResponse.data);
@@ -33,6 +42,6 @@ app.get('/news', async (req, res) => {
 });
 
 // Create an HTTP server
-http.createServer(app).listen(port, () => {
-  console.log(`Server is running on https://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
